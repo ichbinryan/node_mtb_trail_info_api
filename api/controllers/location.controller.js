effada3a94afd63c3e802ec6cdc9e2b3b5fa08a8
Ryan Read <ryan.read@veteransunited.com>
2023-01-09 17:59:42 -0600

    add controller skeleton

---
+++ api/controllers/location.controller.js
@@ -0,0 +1,65 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+    res.status(501).send(
+        {
+            message: "Not yet implemented"
+        }
+    );
+};
+
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+    res.status(501).send(
+        {
+            message: "Not yet implemented"
+        }
+    );
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+    res.status(501).send(
+        {
+            message: "Not yet implemented"
+        }
+    );
+};
+
+// Update a Tutorial by the id in the request
+exports.update = (req, res) => {
+    res.status(501).send(
+        {
+            message: "Not yet implemented"
+        }
+    );
+};
+
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+    res.status(501).send(
+        {
+            message: "Not yet implemented"
+        }
+    );
+};
+
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {
+    res.status(501).send(
+        {
+            message: "Not yet implemented"
+        }
+    );
+};
+
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {
+    res.status(501).send(
+        {
+            message: "Not yet implemented"
+        }
+    );
+};
